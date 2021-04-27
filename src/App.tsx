c456248a42e9f8b67d646c8d17ed2d6db44cdf0a
Salome Goshadze <sally.goshadze@yahoo.com>
2021-04-27 18:41:46 +0400

    Basic setup and design

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useQuery } from "react-query";
+import "./App.css";
+import { Badge, Menu, Layout, Spin, Row, Col, PageHeader } from "antd";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import Item from "./components/Item";
 
-function App() {
+const { Header, Footer, Sider, Content } = Layout;
+
+export type CartItemType = {
+  id: number;
+  title: string;
+  category: string;
+  amount: number;
+  price: number;
+  image: string;
+  description: string;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  const data = await (await fetch("https://fakestoreapi.com/products")).json();
+  return data;
+};
+
+// get total
+
+// add to cart
+const addToCart = (clickedItem: CartItemType) => null;
+// remove item
+
+const App = () => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    "products",
+    getProducts
+  );
+
+  if (isLoading) return <Spin />;
+  if (error) return <h1>Something went wrong:(</h1>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PageHeader style={{ backgroundColor: "#1890ff" }}>
+        <Badge count={0} showZero>
+          <ShoppingCartOutlined
+            style={{
+              fontSize: "25px",
+            }}
+          />
+        </Badge>
+      </PageHeader>
+
+      <Row gutter={[12, 12]} style={{ margin: "2% auto" }}>
+        {data?.map((item) => (
+          <Col span={8} key={item.id} xs={24} sm={24} md={12} lg={12} xl={8}>
+            <Item item={item} addToCart={addToCart} />
+          </Col>
+        ))}
+      </Row>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Item.tsx
@@ -0,0 +1,38 @@
+import { CartItemType } from "../App";
+import { Card, Button } from "antd";
+import { EditOutlined, SettingOutlined } from "@ant-design/icons";
+
+const { Meta } = Card;
+
+type Props = {
+  item: CartItemType;
+  addToCart: (clickedItem: CartItemType) => void;
+};
+
+const Item: React.FC<Props> = ({ item, addToCart }) => {
+  return (
+    <Card
+      style={{ width: 450 }}
+      actions={[
+        <Button disabled>Buy Now</Button>,
+
+        <Button
+          type="primary"
+          onClick={() => {
+            addToCart(item);
+          }}
+        >
+          Add To Cart
+        </Button>,
+      ]}
+    >
+      <Meta
+        avatar={<img height={150} alt={item.title} src={item.image} />}
+        title={`$${item.price}`}
+        description={<h4>{item.title}</h4>}
+      />
+    </Card>
+  );
+};
+
+export default Item;
